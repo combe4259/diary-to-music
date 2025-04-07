9e3bd7727417278084022f5c2e1a8a02d807ec3c
combe4259 <82356080+combe4259@users.noreply.github.com>
2025-04-07 21:48:46 +0900

    Update config.py

--- back/config.py
+++ back/config.py
@@ -1,1 +1,4 @@
-
+CLIENT_ID = "d28959f1c6ade7c33f4655a05e406d6d"
+CLIENT_SECRET = "JLFPTmKUwfIFxL24QKJVvXaeZt9olFqV"
+REDIRECT_URI = "https://flask-login-signup-15no.onrender.com/oauth"
+MYSQL_DATABASE_URI = 'mysql+pymysql://inter4259:mysql4259@mysql-y0lx:3306/db_name'
